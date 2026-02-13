9a1837cd72517de03faf84623b7590353a2798ac
Danh Phan <phandanh2112@gmail.com>
2026-02-13 14:45:40 +0700

    v1.0.4: stop tracking out/

--- out/build/OOP_presets/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/danh.phan-cong/source/Cpp/OOP_Cpp
-# Build directory: C:/Users/danh.phan-cong/source/Cpp/OOP_Cpp/out/build/OOP_presets
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
