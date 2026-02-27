85bd72217901f1e668b690819e871b7602f8df3e
Danh Phan <phandanh2112@gmail.com>
2026-02-27 09:18:03 +0700

    v2.1.0: apply getter/setter

--- src/Class_Declaration_and_Definition/Article.cpp
+++ src/Class_Declaration_and_Definition/Article.cpp
@@ -6,20 +6,38 @@ class Article
 public:
     Article( const string & = "", double = 0.0 );
     virtual ~Article();
+    
+    // accessors methods
+    // getters
+    long getCode() const { return code; }
+    const string & getName() const { return name; }
+    double getPrice() const { return price; }
+    
+    // setters
+    void setName( const string & s )
+    {
+        if ( s.size() < 1 )
+            throw string( "Empty string!" );
+        name = s;
+    }
+    void setPrice( double p )
+    {
+        price = p > 0.0 ? p : 0.0;
+    }
 private:
     long code;
     string name;
     double price;
-    static int countObj;
+    static int countObj;    // static member
+    void setCode() { code = countObj; }
 };
 
-// define static member
-int Article::countObj = 0;
+int Article::countObj = 0; // init
 
-Article::Article( const string & s, double p )
-: name( s ), price( p )
+Article::Article( const string & s, double p ) : name( s )
 {
-    code = countObj;
+    setPrice( p );
+    setCode();
     ++countObj;
     cout << "[" << code << "] " << name << " is created.\n"
     << "This is the #" << countObj << " article!" << endl;
@@ -33,6 +51,9 @@ Article::~Article()
 }
 
 int main() {
-    Article a("law", 123.4);
+    Article a("law");
+    a.setPrice(123.4);
+    cout << a.getPrice() << endl;
+    
     Article b("entertainment", 43.4);
 }
